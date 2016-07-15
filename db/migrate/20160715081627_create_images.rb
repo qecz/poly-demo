e24e3ea411650c8733b6ac93325fe328fd083c40
alceo <alceo@alceo.ru>
2016-07-15 14:03:21 +0300

    commentable image

--- db/migrate/20160715081627_create_images.rb
+++ db/migrate/20160715081627_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
       t.string :url
-
+      t.references :commentable, polymorphic: true #!
       t.timestamps null: false
     end
   end
